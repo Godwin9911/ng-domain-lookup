43ccd6790b9445caf2f8d4264a06bd793c00056d
Godwin9911 <agedah99@gmail.com>
2019-10-14 02:40:16 +0100

    update server file path

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ app.post('/api/checkdomain', (req, res) => {
 
 // serve static assets in prod
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'dist')));
+  app.use(express.static(path.join(__dirname, 'client', 'dist', 'client')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'client', 'index.html'))
   })
 }
 
