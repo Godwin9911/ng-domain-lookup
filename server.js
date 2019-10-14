e49f7b10aebc2a9b12569d22868bdf1bc6ba2899
Godwin9911 <agedah99@gmail.com>
2019-10-14 02:29:53 +0100

    moved type-s dep

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post('/api/checkdomain', (req, res) => {
 
 // serve static assets in prod
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, 'client', 'dist')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
   })
